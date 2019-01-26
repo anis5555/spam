c439ac396c14e687cc2ecd30d5dcb964acc890d1
anis5555 <47055330+anis5555@users.noreply.github.com>
2019-01-26 12:12:06 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get(538665619155976192/538671092626358292)
+setInterval(function() {
+channel.send(احححححححححح سبام رهيب يا زق ايم ذو بسط فورر ايفر);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
